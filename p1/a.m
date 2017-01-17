0b2a9f72c725817b810685e739c3df217086030f
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-17 22:10:28 +0330

    Adds a solution

--- p1/a.m
+++ p1/a.m
@@ -1,8 +1,13 @@
 function weather = forecast(p, n, initial)
     if(n == 1)
-        weather = initial * p
+        weather = initial * p;
     else
-        weather = forecast(p, n - 1, initial) * p
+        weather = forecast(p, n - 1, initial) * p;
     endif
 
 endfunction
+
+today = [0, 1, 0];
+p = [.4 .4 .2; .3 .3 .4; .1 .6 .3];
+weather = forecast(p, 5, today)
+
