21190d826e65b97da2fc7a1d928f3644cae9a8ca
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-17 22:19:27 +0330

    Adds part c

---
+++ p1/c.m
@@ -0,0 +1,14 @@
+function weather = forecast(p, n, initial)
+    if(n == 1)
+        weather = initial * p;
+    else
+        weather = forecast(p, n - 1, initial) * p;
+    endif
+
+endfunction
+
+today = [0, 1, 0];
+p = [.4 .4 .2; .3 .3 .4; .1 .6 .3];
+weather = forecast(p, 5, today)
+
+
