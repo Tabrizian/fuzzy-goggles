809de3a1549d7cea4d237258bf4a61b94b423056
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-18 07:54:48 +0330

    Adds plots

--- p2/programmatic.m
+++ p2/programmatic.m
@@ -17,11 +17,19 @@ endfunction
 
 source_image = "nature.jpg"
 input_image = imread(source_image);
+source_size = getfield(stat(source_image), "size");
 input_image = rgb2gray(input_image);
-for rank = [10, 100, 500, 1000, 2000]
-    compressed_image = compress(rank, input_image);
-    compressed_path = strcat(num2str(rank), ".jpg")
+err = [];
+compression_ratio = [];
+rank = [10, 199, 500, 1000, 2000];
+for rank_item = [10, 100, 500, 1000, 2000]
+    compressed_image = compress(rank_item, input_image);
+    compressed_path = strcat(num2str(rank_item), ".jpg")
     imwrite(compressed_image, compressed_path);
-    err = square_error(input_image, compressed_image);
-    printf("Error is %.73f", err)
+    compressed_size = getfield(stat(compressed_path), "size");
+    compression_ratio = [compressed_size / source_size, compression_ratio];
+    err = [err, mean(meansq(input_image - compressed_image))];
 endfor
+plot(rank, compression_ratio)
+%plot(rank, err)
+
